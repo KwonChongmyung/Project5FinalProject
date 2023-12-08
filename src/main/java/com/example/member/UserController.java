8966883a55c43f18b196a84c0829ba4ff93ce18c
mcwon <21900053@handong.ac.kr>
2023-12-08 11:16:19 +0900

    수정

--- src/main/java/com/example/member/UserController.java
+++ src/main/java/com/example/member/UserController.java
@@ -1,5 +1,21 @@
 package com.example.member;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping(value="/login")
+public class UserController {
+    @Autowired
+    UserServiceImpl service;
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String login(){
+        return "login";
+    }
+    @RequestMapping(value="/loginOK", method=RequestMethod.POST)
+    public String loginCheck(HttpSession session, UserVO vo){
+        String returnURL = "";
+    }
 }
